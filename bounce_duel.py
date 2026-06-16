cf12a2efbacf483ed062284b897301f5b3b898e5
liepechimeid1 <293992029+liepechimeid1@users.noreply.github.com>
2026-06-16 14:26:46 +0200

    Vierde commit - ball movement

--- bounce_duel.py
+++ bounce_duel.py
@@ -18,6 +18,69 @@ FPS = 60
         self.font_medium = pygame.font.Font(None, 24)
         self.font_small = pygame.font.Font(None, 16)
 
+        class Paddle:
+    def __init__(self, x, y, width, height, color):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color = color
+        self.velocity_y = 0
+        self.speed = PADDLE_SPEED
+
+    def update(self, dt, keys, key_up, key_down):
+        # Handle input
+        self.velocity_y = 0
+        if keys[key_up]:
+            self.velocity_y = -self.speed
+        elif keys[key_down]:
+            self.velocity_y = self.speed
+
+        # Move paddle
+        self.rect.y += self.velocity_y * dt
+
+        # Keep in bounds
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > WINDOW_HEIGHT:
+            self.rect.bottom = WINDOW_HEIGHT
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, self.rect)
+
+self.paddle1 = Paddle(20, WINDOW_HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT, CYAN)
+ self.paddle2 = Paddle(WINDOW_WIDTH - 20 - PADDLE_WIDTH, WINDOW_HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT, MAGENTA)
+
+         self.paddle1.update(dt, keys, pygame.K_w, pygame.K_s)
+        self.paddle2.update(dt, keys, pygame.K_UP, pygame.K_DOWN)
+
+        class Ball:
+    def __init__(self, x, y):
+        self.rect = pygame.Rect(x - BALL_SIZE // 2, y - BALL_SIZE // 2, BALL_SIZE, BALL_SIZE)
+        self.color = YELLOW
+        self.velocity_x = 0
+        self.velocity_y = 0
+        self.round_start_time = 0
+
+    def set_velocity_from_angle(self, angle, speed, direction_x=1):
+        """Set velocity based on angle and speed"""
+        rad = math.radians(angle)
+        self.velocity_x = math.cos(rad) * speed * direction_x
+        self.velocity_y = math.sin(rad) * speed
+
+    def update(self, dt):
+        # Move ball
+        self.rect.x += self.velocity_x * dt
+        self.rect.y += self.velocity_y * dt
+
+        # Bounce off top and bottom
+        if self.rect.top <= 0:
+            self.rect.top = 0
+            self.velocity_y = abs(self.velocity_y)
+        if self.rect.bottom >= WINDOW_HEIGHT:
+            self.rect.bottom = WINDOW_HEIGHT
+            self.velocity_y = -abs(self.velocity_y)
+
+    def draw(self, surface):
+        pygame.draw.circle(surface, self.color, self.rect.center, BALL_SIZE // 2)
+
         while running:
           # Handle events
             for event in pygame.event.get():
